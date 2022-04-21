ce77f02fa30395cb231e8760d3c3cfb49fd097fc
Vitor M. Almeida <83554161+vitoalmeida@users.noreply.github.com>
2022-04-20 22:17:33 -0300

    Feat/update readme (#2)
    * Add proposital errors
    
    * Fix svg files
    
    * Add dictionary
    
    * Add proposital errors
    
    * Add extra dictionaries
    
    * Add extra dictionaries
    
    * Add composer check
    
    * Add composer check
    
    * Add composer check
    
    * Add composer check
    
    * Add composer check
    
    * Add composer check
    
    * Add composer check
    
    * Add composer check
    
    * Teste
    
    * Teste2
    
    * Add some stuffs

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,15 +10,14 @@ interface Props {
 
 const Button: React.FC<Props> = ({ text = 'BUSCAR', isSubmitting = false }) => {
   return (
-    <a href="#">
-      <button
-        type="submit"
-        disabled={isSubmitting}
-        className="flex hover:scale-95 duration-300 w-full h-14 rounded-2xl bg-dark-green text-xl text-white font-bold border-[#00b587] border-b-[0.4rem] active:border-b-[0.2rem] justify-center items-center"
-      >
-        {isSubmitting ? <Spinner color="#FFF" size={18} /> : text}
-      </button>
-    </a>
+    <button
+      data-cy="submitButton"
+      type="submit"
+      disabled={isSubmitting}
+      className="flex hover:scale-95 duration-300 w-full h-14 rounded-2xl bg-dark-green text-xl text-white font-bold border-[#00b587] border-b-[0.4rem] active:border-b-[0.2rem] justify-center items-center"
+    >
+      {isSubmitting ? <Spinner color="#FFF" size={18} /> : text}
+    </button>
   );
 };
 
